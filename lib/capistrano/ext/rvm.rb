cc3c5eaee1312dd211c8b3d39095fdad6094f70f
Marcin Nowicki <pr0d1r2@gmail.com>
2011-11-14 19:04:45 +0100

    Add description to task.
    Update Manifest.

--- lib/capistrano/ext/rvm.rb
+++ lib/capistrano/ext/rvm.rb
@@ -2,11 +2,10 @@ Capistrano::Configuration.instance.load do
 
   namespace :rvm do
     namespace :gemset do
-
+      desc "create RVM gemset"
       task :create, :roles => :app, :except => { :no_release => true } do
         run "rvm gemset create '#{rvm_gemset}'"
       end
-
     end
   end
 
